64c07a3a58a89f830fc3888ec0daab59075bd286
Gary Chang <gary.chang.dev@gmail.com>
2020-08-09 02:12:58 -0400

    Unity UI full commit

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    // Singleton
+    public static GameSettings instance;
+
+    public int difficulty;
+    public float soundVolume;
+    public float musicVolume;
+    public string playerName;
+
+    void Awake()
+    {
+        // Setup singleton
+        if (instance != null && instance != gameObject)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+
+    public void SaveData(SettingsData data)
+    {
+        difficulty = data.difficulty;
+        soundVolume = data.soundVolume;
+        musicVolume = data.musicVolume;
+        playerName = data.playerName;
+    }
+    public SettingsData LoadData()
+    {
+        SettingsData data;
+        data.difficulty = difficulty;
+        data.soundVolume = soundVolume;
+        data.musicVolume = musicVolume;
+        data.playerName = playerName;
+        return data;
+    }
+}
+
